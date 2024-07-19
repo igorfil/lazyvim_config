09e659771ade002787dbed768a82cbab6e0527a7
Igor Fil <igor.v.fil@gmail.com>
2024-07-19 22:06:11 +0000

    Add elixir plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.elixir" },
+    { import = "lazyvim.plugins.extras.lang.erlang" },
     -- import/override with your plugins
     { import = "plugins" },
   },
